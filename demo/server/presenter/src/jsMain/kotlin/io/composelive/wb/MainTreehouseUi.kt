82a00007006862cd3a95f98977a9473c9e17ee0b
tret9 <tret95@mail.ru>
2026-02-02 20:56:34 +0300

    Fix MainTreehouseUi

--- demo/server/presenter/src/jsMain/kotlin/io/composelive/wb/MainTreehouseUi.kt
+++ demo/server/presenter/src/jsMain/kotlin/io/composelive/wb/MainTreehouseUi.kt
@@ -4,9 +4,9 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.rememberCoroutineScope
 import app.cash.redwood.compose.ConsumeInsets
 import app.cash.redwood.treehouse.TreehouseUi
-import io.composelive.designsystem.core.compose.Root
-import io.composelive.presenter.Main
 import io.composelive.network.HttpClient
+import io.composelive.nodes.foundation.compose.Root
+import io.composelive.presenter.Main
 import io.composelive.presenter.presentation.Navigator
 import kotlinx.serialization.json.Json
 
